f0afa33305294545b21e863e16e3701c367c6c58
96StanleyLee <96stanleylee@gmail.com>
2020-03-28 15:17:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 // Code your solution here:
-function driversWithRevenueOver(array, number){
-    return array.filter(function (driver){
-        return driver.revenue > number
-    })
-}
-
-function driverNamesWithRevenueOver(array, number){
-    let drivers = driversWithRevenueOver(array, number)
-    return drivers.map(function (drivers){
-        return drivers.name 
+function driversWithRevenueOver(driver, revenue){
+    return driver.filter(function (drivers){
+        return drivers.revenue > revenue
     })
 }
 
 
-function exactMatch(array, attribute){
-
-
-
-    return array.filter(function (driver){
-        for(const key in driver){
-
-            return driver[key] == attribute[key]
-
-        }
+function driverNamesWithRevenueOver(driver, revenue){
+    const drivers_over_revenue = driversWithRevenueOver(driver, revenue)
+    return drivers_over_revenue.map(function (driver){ //the function (driver) is setting each unique object to be a driver
+        return driver.name // similar to rubys Person.all.each do |person|, the first person is person and it will loop.s
     })
 }
 
 
-function exactMatchToList(array, attribute){
+// function exactMatch(driver, object){
+//     return driver.filter(function (drivers){
+//         for(const key in object){
+//             return drivers[key] === object[key]
+//         }
+//     })
+// }
 
-let filtered = exactMatch(array, attribute)
-    return filtered.map(function (drivers){
-        return drivers.name
+function exactMatch(driver, object){
+
+    let key = Object.keys(object)[0]
+    let value = Object.values(object)[0]
+    return driver.filter(function (drivers){
+        return drivers[key] === value
+    })
+}
+
+
+
+function exactMatchToList(driver, object){
+    let drivers = exactMatch(driver, object)
+    return drivers.map(function (driver){
+        return driver.name
     })
 }
